95c81d21f33c8d12ea7b0c68a6a3b57214b981fb
suriya-a9 <uriyaprakash@gmail.com>
2025-12-23 13:37:24 +0530

    After added success true

--- backend/modules/user/userService/userService.controller.js
+++ backend/modules/user/userService/userService.controller.js
@@ -84,8 +84,8 @@ exports.userServiceList = async (req, res, next) => {
             });
         }
 
-        const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 1;
+        const page = parseInt(req.query.page) || 10;
+        const limit = parseInt(req.query.limit) || 10;
         const skip = (page - 1) * limit;
 
         const totalCount = await UserService.countDocuments({ userId });
